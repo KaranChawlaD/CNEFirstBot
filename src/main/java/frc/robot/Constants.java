b157612aa7c017ac372b7324b71fcb4127922c9a
KaranChawlaD <karan.chawlad@gmail.com>
2023-08-28 21:58:57 -0400

    Testing Simulations

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -32,8 +32,10 @@ public final class Constants {
     public static final int DRIVE_FRONT_RIGHT = 4;
     public static final int DRIVE_BACK_LEFT = 3;
     public static final int DRIVE_BACK_RIGHT = 5;
-    public static final int LEFT_ENCODER_A = 1;
-    public static final int LEFT_ENCODER_B = 2;
+    public static final int LEFT_ENCODER_A = 0;
+    public static final int LEFT_ENCODER_B = 1;    
+    public static final int RIGHT_ENCODER_A = 2;
+    public static final int RIGHT_ENCODER_B = 3;
     public static final int DRIVE_AXIS = 1;
     public static final int TURN_AXIS = 4;
     public static final double TURN_PROPORTION = 0.7;
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,10 +5,13 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
@@ -35,6 +38,11 @@ public class DriveSubsystem extends SubsystemBase {
 
   private final AHRS gyro = new AHRS(SPI.Port.kMXP);
   private final Encoder encoderLeftDrive = new Encoder(DriveConstants.LEFT_ENCODER_A, DriveConstants.LEFT_ENCODER_B);
+  private final Encoder encoderRightDrive = new Encoder(DriveConstants.RIGHT_ENCODER_A, DriveConstants.RIGHT_ENCODER_B);
+  private final Field2d field = new Field2d();
+  DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(gyro.getRotation2d(),
+      encoderLeftDrive.getDistance(), encoderRightDrive.getDistance());
+  Pose2d pose;
 
   public double getEncoderDrivePosition() {
     return (encoderLeftDrive.getDistance());
@@ -76,6 +84,7 @@ public class DriveSubsystem extends SubsystemBase {
     driveRight.setInverted(true);
     turnController.setTolerance(DriveConstants.TURN_CONTROLLER_POSITION_TOLERANCE,
         DriveConstants.TURN_CONTROLLER_VELOCITY_TOLERANCE);
+    SmartDashboard.putData("Field", field);
   }
 
   @Override
@@ -87,7 +96,8 @@ public class DriveSubsystem extends SubsystemBase {
     SmartDashboard.putNumber("Encoder Left Distance", encoderLeftDrive.getDistance());
     SmartDashboard.putNumber("PID Right Turn Speed", turnController.calculate(getGyroYaw(), 90));
     SmartDashboard.putNumber("PID Left Turn Speed", turnController.calculate(getGyroYaw(), -90));
-
+    pose = odometry.update(gyro.getRotation2d(), encoderLeftDrive.getDistance(), encoderRightDrive.getDistance());
+    field.setRobotPose(pose);
   }
 
   public void setMotor(double forwardSpeed, double turnSpeed) {
